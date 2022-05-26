bcc559bcf828cc1b232300157ac4cc7225e74d2f
Alireza Alibeiki <alibeikialireza@gmail.com>
2022-05-26 09:54:35 +0430

    feat(nuxt): add PersianDate module
    when the 'PersianDate' option is true, the PersianDate module will install.

--- src/nuxt/index.ts
+++ src/nuxt/index.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, createResolver } from '@nuxt/kit';
+import { defineNuxtModule, createResolver, installModule } from '@nuxt/kit';
 
 export default defineNuxtModule({
   meta: {
@@ -8,12 +8,21 @@ export default defineNuxtModule({
       nuxt: '^3.0.0',
     },
   },
+  setup(options, nuxt) {
+    const { include } = nuxt.options.vite.optimizeDeps!;
+    nuxt.options.vite.optimizeDeps!.include = [
+      ...(include || []),
+      '@alireza-ab/persian-date',
+    ];
+
+    if (options.PersianDate) installModule('@alireza-ab/persian-date');
+  },
   hooks: {
     'components:dirs': (dirs) => {
       const { resolve } = createResolver(import.meta.url);
 
       dirs.push({
-        path: resolve('./components'),
+        path: resolve('../src/components'),
         pattern: '**/*.vue',
       });
     },
